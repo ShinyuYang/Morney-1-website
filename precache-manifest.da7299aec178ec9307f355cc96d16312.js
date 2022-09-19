f561c7ee6fe5bb6f83d0435c46a4652558b69913
ShinyuYang <1449404535@qq.com>
2022-09-19 15:32:25 +0800

    deploy

---
+++ precache-manifest.da7299aec178ec9307f355cc96d16312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebbfb6fd0cdc8dc0637",
+    "url": "/Morney-1-website/css/app.179a2144.css"
+  },
+  {
+    "revision": "70dd91fa028a05b8164407078b8a62d4",
+    "url": "/Morney-1-website/index.html"
+  },
+  {
+    "revision": "2ebbfb6fd0cdc8dc0637",
+    "url": "/Morney-1-website/js/app.f5b0f6fa.js"
+  },
+  {
+    "revision": "208b896ada9762295d36",
+    "url": "/Morney-1-website/js/chunk-vendors.f7efc6d8.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/Morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-1-website/robots.txt"
+  }
+]);
